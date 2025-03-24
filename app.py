4978b766aa84e8e2ee83d3f49c207b4df1575d25
SorokaBelaboka <Kasianov.Artem@urfu.me>
2025-03-24 19:32:35 +0500

    Версия 0.2

--- app.py
+++ app.py
@@ -1,84 +1,106 @@
+# Импорт необходимых библиотек
 import json
-from flask import Flask, request, jsonify
-from tinkoff.invest import Client, OrderDirection, OrderType, InstrumentType, FindInstrumentRequest
+from http.client import responses
 
+from flask import Flask, request, jsonify  # Веб-фреймворк и инструменты для работы с HTTP
+from tinkoff.invest import Client, OrderDirection, OrderType, InstrumentIdType, \
+    InstrumentRequest, MoneyValue, Quotation  # Официальный SDK Тинькофф Инвестиций
+from tinkoff.invest.sandbox.client import SandboxClient
+
+# Токен доступа к API Тинькофф Инвестиций (SANDBOX)
 TOKEN = "t.Gk6qrpcVv87MYW8ZPgUOO7dKVV-GLQKtKAeymLEmZaGA6UTi9LseC0zvkCZ4GrgRdnrNFxfuwTgjT1V4xV-oJA"
 
+# Инициализация Flask приложения
 app = Flask(__name__)
-account_id = None
-ticker_to_figi_uid_library = {}
+account_id = None  # Здесь будет храниться ID песочного аккаунта
+ticker_to_figi_uid_library = {}  # Словарь для соответствия тикеров и идентификаторов инструментов
 
 
 def initialize_libraries(tokens_to_figis_file):
+    """Загружает данные из JSON-файла в глобальный словарь ticker_to_figi_uid_library"""
     with open(tokens_to_figis_file, 'r', encoding='utf-8') as json_file:
         global ticker_to_figi_uid_library
         ticker_to_figi_uid_library = json.load(json_file)
 
-def adding_new_figi_to_uid_pair(ticker, figi, uid, file):
 
-    global ticker_to_figi_uid_library
-    ticker_to_figi_uid_library[ticker] = {"figi": figi, "uid": uid}  # Обновляем библиотеку
-
-    with open(file, 'w', encoding='utf-8') as json_file:  # Записываем в файл
-        json.dump(ticker_to_figi_uid_library, json_file, ensure_ascii=False, indent=4)
-        print(f"Записано в файл {file}: {ticker} => {figi}, UID: {uid}")
-
-def get_instrument_id(client, ticker):
-    # Проверка, есть ли тикер в библиотеке
+def get_full_instrument(client, ticker):
+    """Получает полную информацию об инструменте по тикеру через API"""
+    # Проверка наличия тикера в библиотеке
     if ticker not in ticker_to_figi_uid_library:
         print(f"Тикер {ticker} не найден в библиотеке")
         return None
 
     figi = ticker_to_figi_uid_library[ticker]["figi"]
-    uid = ticker_to_figi_uid_library[ticker].get("uid", None)
 
-    # Если uid уже есть, возвращаем его
-    if uid:
-        return uid
-
-    response = client.instruments.find_instrument(
-        query=figi,
-        instrument_kind=InstrumentType.INSTRUMENT_TYPE_SHARE,
-        api_trade_available_flag=True
-    )
     try:
-        instrument = response.instruments
+        # Запрос к API Тинькофф
+        response_full = client.instruments.get_instrument_by(
+            id_type = InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
+            id=figi
+        )
+        instrument = response_full.instrument
         if instrument:
-            current_uid = instrument[0].uid
-            print(f"Найден новый инструмент - TICKER: {ticker}; FIGI: {figi}; UID: {current_uid}")
-            adding_new_figi_to_uid_pair(ticker, figi, current_uid, 'tokens_figi_uid.json')
-            return current_uid
+            print(
+                f"Инструмент найден: TICKER={instrument.ticker} FIGI={instrument.figi}, LOT={instrument.lot}, UID={instrument.uid}")
+            return instrument
         else:
-            print(f"Инструмент с тикером {ticker} не найден в базе.")
+            print(f"Инструмент с TICKER={ticker}, FIGI={figi} не найден.")
             return None
     except Exception as e:
-        print(f"Ошибка при поиске инструмента: {str(e)}")
+        print(f"Ошибка при запросе полного инструмента: {str(e)}")
         return None
 
-def place_order(client, ticker, direction, quantity, price=None):
-    instrument_id = get_instrument_id(client, ticker)
-    if not instrument_id:
-        return {"error": f"Инструмент с тикером {ticker} не найден."}
 
+def get_quantity(expected_sum, instrument_lot, price=None):
+    """Рассчитывает количество лотов для ордера"""
     try:
         if price is not None:
+            # Для лимитных ордеров: рассчитываем количество на основе цены
+            price_val = float(price.units)
+            units = expected_sum / price_val
+            quantity = int(units) // instrument_lot * instrument_lot
+        else:
+            # Для рыночных ордеров: используем ожидаемую сумму напрямую
+            quantity = int(expected_sum) // instrument_lot * instrument_lot
+
+        # Проверка минимального количества
+        if quantity <= 0:
+            print(f"Рассчитанное количество лотов меньше или равно 0 = {quantity}")
+            return 0
+
+        return quantity
+    except Exception as e:
+        print(f"Ошибка при расчёте количества: {str(e)}")
+        return 0
+
+
+def place_order(client, ticker, direction, expected_sum, price=None):
+    """Размещает ордер через API Тинькофф"""
+    current_instrument = get_full_instrument(client, ticker)
+    if current_instrument is None:
+        return {"error": "Не найден инструмент"}, 400
+
+    # Расчет количества лотов
+    quantity = get_quantity(expected_sum, current_instrument.lot, price)
+
+    try:
+        # Определение типа ордера (рыночный/лимитный)
+        if price is not None:
             order_type = OrderType.ORDER_TYPE_LIMIT
-            price_dict = {"units": int(price), "nano": 0}
-            print(
-                f"Размещаем лимитный ордер: instrument_id={instrument_id}, direction={direction}, quantity={quantity}, price={price_dict}, account_id={account_id}")
+            print(f"Размещаем лимитный ордер с ценой {price}...")
         else:
             order_type = OrderType.ORDER_TYPE_MARKET
-            price_dict = None
-            print(
-                f"Размещаем рыночный ордер: instrument_id={instrument_id}, direction={direction}, quantity={quantity}, account_id={account_id}")
+            price_value = None
+            print(f"Размещаем рыночный ордер...")
 
+        # Отправка ордера в песочницу
         response = client.orders.post_order(
-            instrument_id=instrument_id,
+            instrument_id=current_instrument.uid,
             quantity=quantity,
             direction=direction,
             account_id=account_id,
             order_type=order_type,
-            price=price_dict if order_type == OrderType.ORDER_TYPE_LIMIT else None
+            price=price if order_type == OrderType.ORDER_TYPE_LIMIT else None
         )
         print(f"Ордер успешно размещён: {response}")
         return {"order_id": response.order_id}
@@ -86,55 +108,93 @@ def place_order(client, ticker, direction, quantity, price=None):
         print(f"Ошибка при размещении ордера: {str(e)}")
         return {"error": str(e)}
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    """Обработчик входящих вебхуков"""
     data = request.json
     print("Received webhook data:", data)
 
+    # Извлечение параметров из запроса
     ticker = data.get("ticker")
     direction = data.get("direction")
-    position_size = data.get("position_size")
-    price = data.get("price")
+    price_value = data.get("price")
 
-    if not direction or not position_size or not ticker:
-        return jsonify({"error": "Недостаточно данных в вебхуке: требуется direction, position_size и ticker."}), 400
+    # Преобразуем цену в float для точности
+    price_float = float(price_value)
 
+    # Получаем целую часть и дробную (нано-часть)
+    units = int(price_float)
+    nano = round((price_float - units) * 1_000_000)
+
+    # Создаем объект Quotation
+    price = Quotation(
+        units=units,
+        nano=nano
+    )
+    print(price)
+    expected_sum = int(data.get("expected_sum"))
+
+    # Валидация входных данных
+    if not direction or not expected_sum or not ticker:
+        return jsonify({"error": "Недостаточно данных"}), 400
+
+    # Преобразование направления ордера
     if direction.lower() == "buy":
         direction_order = OrderDirection.ORDER_DIRECTION_BUY
     elif direction.lower() == "sell":
         direction_order = OrderDirection.ORDER_DIRECTION_SELL
     else:
-        return jsonify({"error": "Неподдерживаемое направление: " + direction}), 400
+        return jsonify({"error": "Неподдерживаемое направление"}), 400
 
-    quantity = int(position_size)
-
-    with Client(TOKEN) as client:
-        result = place_order(client, ticker, direction_order, quantity, price)
+    # Размещение ордера через SDK
+    with SandboxClient(TOKEN) as client:
+        result = place_order(client, ticker, direction_order, expected_sum, price)
         if "error" in result:
             return jsonify(result), 400
         return jsonify(result)
 
-def main():
+
+def initialize_sandbox_account(client):
     global account_id
+    accounts = client.sandbox.get_sandbox_accounts()
 
-    initialize_libraries('tokens_figi_uid.json')
+    # Проверка наличия хотя бы одного аккаунта
+    if accounts.accounts:
+        # Если аккаунт есть, подключаемся к первому найденному
+        account_id = accounts.accounts[0].id
+        print(f"Подключено к существующему счету: {account_id}")
 
-    with Client(TOKEN) as client:
-        accounts = client.sandbox.get_sandbox_accounts()
-        print("Существующие песочные аккаунты:", accounts)
+        balance = client.sandbox.sandbox_pay_in(
+            account_id=account_id,
+            amount={"units": 0, "nano": 0}
+        )
+        print(f"Баланс: {balance}")
 
-        if accounts.accounts:
-            for acc in accounts.accounts:
-                print(f"Закрываем аккаунт: {acc.id}")
-                client.sandbox.close_sandbox_account(account_id=acc.id)
-
+    else:
+        # Если аккаунтов нет, создаем новый и пополняем его
         sandbox_account = client.sandbox.open_sandbox_account()
         account_id = sandbox_account.account_id
         print(f"Создан новый счет: {account_id}")
+        # Пополнение баланса
+        client.sandbox.sandbox_pay_in(
+            account_id=account_id,
+            amount=MoneyValue(units=200000, nano=0, currency="rub")  # 200 000 рублей
+        )
+        print(f"Добавлены средства на счет {account_id}")
 
-        client.sandbox.sandbox_pay_in(account_id=account_id, amount={"units": 200000, "nano": 0})
-        print(f"Добавлено 100,000 рублей на счёт {account_id}")
+def main():
+    """Основная функция инициализации"""
+    global account_id
 
+    # Инициализация библиотеки тикеров
+    initialize_libraries('tokens_figi_uid.json')
+
+    # Работа с песочными аккаунтами
+    with SandboxClient(TOKEN) as client:
+        initialize_sandbox_account(client) # Инициализация аккаунта песочницы
+
+    # Запуск веб-сервера
     app.run(host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
--- test_2.bat
+++ test_2.bat
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d "{\"direction\":\"buy\",\"price\":323,\"time\":\"2025-03-31 12:00:00\",\"ticker\":\"SBER\",\"position_size\":10.0}" http://10.0.4.110:5000/webhook
+curl -X POST -H "Content-Type: application/json" -d "{\"ticker\":\"SBER\",\"direction\":\"buy\",\"price\":3.0,\"expected_sum\":100.0,\"time\":\"2025-03-31 12:00:00\"}" http://10.0.4.110:5000/webhook
